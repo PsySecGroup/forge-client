fa8aa5b064a95a3127347fd8ebb15357b51ec24b
HyperCrowd <info@hypercrowd.org>
2025-03-22 16:31:20 -0400

    big migration

--- src/old/concepts/register.ts
+++
@@ -1,9 +0,0 @@
-// (-->) Import new concept default states here
-import { defaultState as uiDefaults } from './ui'
-import { defaultState as navigationDefaults } from './navigation'
-
-export default {
-  // (-->) Add default states here
-  ...uiDefaults,
-  ...navigationDefaults
-}
