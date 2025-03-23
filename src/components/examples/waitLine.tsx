cd2b911c72dffeaff4ae4ee3d32929fa26db8962
HyperCrowd <info@hypercrowd.org>
2025-03-22 20:33:14 -0400

    splittig out components

--- src/components/examples/waitLine.tsx
+++
@@ -1,9 +0,0 @@
-import { Box, LinearProgress } from "@suid/material";
-
-export default function IndeterminateExample() {
-  return (
-    <Box sx={{ width: "100%" }}>
-      <LinearProgress />
-    </Box>
-  );
-}
